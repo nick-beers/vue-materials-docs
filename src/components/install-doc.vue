42cc8ac65b1d4ef0261c67cdde8a37480c3d6ae7
Nick Beers <nicholas.beers@gmail.com>
2017-05-17 12:45:41 -0400

    Updating install docs to reflect actual installation and usage

--- src/components/install-doc.vue
+++ src/components/install-doc.vue
@@ -2,23 +2,23 @@
     <m-grid s12>
         <h4>NPM</h4>
         <markup html='
-        npm install vue-materials'
+        npm install @edge_blade/vue-materials'
         ></markup>
         <h4>JavaScript</h4>
         <markup html='
         import Vue from "vue"
-        import Materials from "vue-materials"
+        import Materials from "@edge_blade/vue-materials"
         Vue.use(Materials)'
         ></markup>
         <h4>HTML</h4>
         <markup html='
         <head>
             <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-            <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.css" rel="stylesheet">
         </head>
         <body>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.js"></script>
         </body>
         '
         ></markup>
