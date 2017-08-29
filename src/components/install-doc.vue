b36404e62c6fc21f4cc2cc6bf1e7a4836edb7d22
Nick Beers <nicholas.beers@gmail.com>
2017-08-29 14:13:57 -0400

    Missed updating the references that the project was built on

--- src/components/install-doc.vue
+++ src/components/install-doc.vue
@@ -1,6 +1,6 @@
 <template>
     <m-grid s12>
-        <h4>NPM <i style="font-size: 16px;">Based on 1.5.0 of @edge_blade/vue-materials</i></h4>
+        <h4>NPM <i style="font-size: 16px;">Based on 1.6.0 of @edge_blade/vue-materials</i></h4>
         <markup html='
         npm install @edge_blade/vue-materials'
         ></markup>
@@ -14,11 +14,11 @@
         <markup html='
         <head>
             <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-            <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.css" rel="stylesheet">
         </head>
         <body>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.js"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.js"></script>
         </body>
         '
         ></markup>
